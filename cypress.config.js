d5e3ae96e4bd7130f46d42295274bb96fa83e882
Ademola Bhadmus <sanguine@Ademolas-MBP.lan>
2024-05-26 16:36:12 +0100

    added mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,25 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'iframe-tests',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     viewportHeight: 960,
     viewportWidth: 1538,
     chromeWebSecurity: false,
     includeShadowDom: true,
+    retries:{
+      runMode: 1,
+      openMode: 1,
+    },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
